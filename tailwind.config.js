13ba49a475093ecefe80d05fc8c0e7bbd726c368
Ney Barbosa <esmererneyolivera@gmail.com>
2024-12-16 17:53:45 -0500

    feat: agregar nuevo tamaño de fuente

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'xxs': '10px',  // Tamaño personalizado más pequeño que 10px
+      },
+    },
   },
   plugins: [],
 };
