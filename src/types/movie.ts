b4c48a70299af81762a35d92efadf0ce9fe09fe4
Ney Barbosa <esmererneyolivera@gmail.com>
2024-12-16 16:40:48 -0500

    feat: mejorar tipados

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,12 +1,18 @@
+import { Gender } from "./gender";
+
 export interface Movie {
-    id: number;
-    title: string;
-    poster_path: string;
-    release_date: string;
-    vote_average: number;
-    genres: any[];
-    backdrop_path?: string; // Opcional, ya que no todas las películas pueden tener un backdrop
-    overview?: string; // Opcional, si quieres incluir una descripción breve de la película
-    genre_ids?: number[]; // Opcional, para obtener los géneros de la película
-    original_language?: string; // Opcional, para el idioma original de la película
-}  
+  id: number;
+  title: string;
+  poster_path: string;
+  release_date: string;
+  vote_average: number;
+  description: string;
+  image: string;
+  runtime: number;
+  trailer_url: string;
+  genres: Gender[];
+  backdrop_path?: string; // Opcional, ya que no todas las películas pueden tener un backdrop
+  overview?: string; // Opcional, si quieres incluir una descripción breve de la película
+  genre_ids?: number[]; // Opcional, para obtener los géneros de la película
+  original_language?: string; // Opcional, para el idioma original de la película
+}
