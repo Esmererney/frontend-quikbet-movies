0d0e7580e05ca2e9d7661684895ef5fe91e72c7b
Ney Barbosa <esmererneyolivera@gmail.com>
2024-12-16 16:40:25 -0500

    feat: agregar nueva pagina para listar favoritos

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,23 +1,23 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from "react";
 import { Movie } from "../../types/movie"; // Asegúrate de tener tu interfaz Movie definida.
 import MoviesRender from "../../component/MoviesRender"; // Componente para marcar como favorito.
 
-const FavoritesPage = () => {
+const FavoritesPage = (): JSX.Element => {
   const [favoriteMovies, setFavoriteMovies] = useState<Movie[]>([]);
 
   // Recuperar películas favoritas del localStorage
   useEffect(() => {
     const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
-    const fetchMovies = async () => {
+    const fetchMovies = async (): Promise<void> => {
       const moviesData: Movie[] = await Promise.all(
         favorites.map(async (movieId: number) => {
           const response = await fetch(
-            `https://api.themoviedb.org/3/movie/${movieId}?api_key=c1aba48feaff17750be529bbf4cda3ee`
+            `https://api.themoviedb.org/3/movie/${movieId}?api_key=c1aba48feaff17750be529bbf4cda3ee`,
           );
-          const data = await response.json();
+          const data: Movie[] = await response.json();
           return data;
-        })
+        }),
       );
       setFavoriteMovies(moviesData);
     };
@@ -28,7 +28,7 @@ const FavoritesPage = () => {
   if (favoriteMovies.length === 0) {
     return (
       <div className="p-4">
-        <h1 className="text-2xl font-bold">Your Favorite Movies</h1>
+        <h1 className="text-2xl font-bold">Your favorite movies</h1>
         <p>No favorite movies found.</p>
       </div>
     );
@@ -36,12 +36,12 @@ const FavoritesPage = () => {
 
   return (
     <div className="p-4">
-    <h1 className="text-4xl font-extrabold text-center text-gradient bg-gradient-to-r from-neutral-700 via-neutral-800 to-neutral-900 bg-clip-text text-transparent mb-6">
-      Your Favorite Movies
-    </h1>
+      <h1 className="text-4xl font-extrabold text-center text-gradient bg-gradient-to-r from-neutral-700 via-neutral-800 to-neutral-900 bg-clip-text text-transparent mb-6">
+        Your Favorite Movies
+      </h1>
 
       <div className="">
-          <MoviesRender movies={favoriteMovies} />
+        <MoviesRender movies={favoriteMovies} />
       </div>
     </div>
   );
